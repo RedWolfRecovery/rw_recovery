f52df2ce2e7c1850cb6080d7874773ca3baea2cb
oshmoun <oshmoun100@googlemail.com>
2018-04-10 17:45:55 +0200

    partitionmanager: signal when Mount_To_Decrypt partitions are mounted
    Enable a property when mounting partitions required for decryption, and disable when they are unmounted.
    This helps init scripts to react to such an event appropriately, for example by starting services critical for decryption that reside on said partitions.
    
    Change-Id: Ib6fa1e5c903d9c800fb145d582ced6cc7e0ab5b9

--- partitionmanager.cpp
+++ partitionmanager.cpp
@@ -1645,6 +1645,7 @@ int TWPartitionManager::Decrypt_Device(string Password) {
 			(*iter)->Mount(true);
 		}
 	}
+	property_set("twrp.mount_to_decrypt", "1");
 
 	property_get("ro.crypto.state", crypto_state, "error");
 	if (strcmp(crypto_state, "error") == 0) {
@@ -1714,6 +1715,7 @@ int TWPartitionManager::Decrypt_Device(string Password) {
 			(*iter)->UnMount(false);
 		}
 	}
+	property_set("twrp.mount_to_decrypt", "0");
 
 	if (pwret != 0) {
 		gui_err("fail_decrypt=Failed to decrypt data.");
