5a8b626140d939eed6494e49491d55e918f4c694
dadi11 <dadi.sikorski@gmail.com>
2018-04-28 15:29:49 +0200

    rw: update unofficial builds version

--- Android.mk
+++ Android.mk
@@ -43,7 +43,7 @@ TARGET_RECOVERY_GUI := true
 ifneq ($(TW_DEVICE_VERSION),)
     LOCAL_CFLAGS += -DTW_DEVICE_VERSION='"$(TW_DEVICE_VERSION)"'
 else
-    LOCAL_CFLAGS += -DTW_DEVICE_VERSION='"026-Unofficial"'
+    LOCAL_CFLAGS += -DTW_DEVICE_VERSION='"027-Unofficial"'
 endif
 
 DEVICE := $(subst omni_,,$(TARGET_PRODUCT))
