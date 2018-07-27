b91a7a537ffe79f598e8655da45a6d3d806ad44d
Ethan Yonker <dees_troy@teamw.in>
2018-07-27 11:49:24 -0500

    Fix installing OTAs without decrypting
    Change-Id: Ica0c4033bc7148b1313b1671e91c5b92b09064c6

--- twrp.cpp
+++ twrp.cpp
@@ -319,11 +319,9 @@ int main(int argc, char **argv) {
 		TWFunc::Fixup_Time_On_Boot();
 
 	// Run any outstanding OpenRecoveryScript
-	if (DataManager::GetIntValue(TW_IS_ENCRYPTED) == 0 && TWFunc::Path_Exists(SCRIPT_FILE_TMP) || TWFunc::Path_Exists(SCRIPT_FILE_CACHE)) {
-	    TWFunc::Start_redwolf();
+	if ((DataManager::GetIntValue(TW_IS_ENCRYPTED) == 0 || SkipDecryption) && (TWFunc::Path_Exists(SCRIPT_FILE_TMP) || TWFunc::Path_Exists(SCRIPT_FILE_CACHE))) {
 		OpenRecoveryScript::Run_OpenRecoveryScript();
 	}
-	
 
 #ifdef TW_HAS_MTP
 	char mtp_crash_check[PROPERTY_VALUE_MAX];
